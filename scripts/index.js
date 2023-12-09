0afa5a7583f978372963decb9e47e201e9124b1e
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-12-09 00:07:06 -0600

    First Commit

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var h1Element = document.getElementById("hero__h1");
+    h1Element.style.opacity = "1";
+    h1Element.style.transform = "translateY(0)";
+});
