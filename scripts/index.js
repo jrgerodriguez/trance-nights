d5e066f206cfc248a73948d369b21bcbb65aaa88
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-03-01 10:43:29 -0600

    tn site updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var h1Element = document.getElementById("hero__h1");
-    h1Element.style.opacity = "1";
-    h1Element.style.transform = "translateY(0)";
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     var h1Element = document.getElementById("hero__h1");
+//     h1Element.style.opacity = "1";
+//     h1Element.style.transform = "translateY(0)";
+// });
 
 
 function changeIcon() {
